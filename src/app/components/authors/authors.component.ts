af1e5d040b04d7337138a13427b7eb10974d8600
Natalia Kedziora <natalia.kedziora@hrs.com>
2017-11-03 17:02:12 +0100

    Added conditional class (class binding example)

--- src/app/components/authors/authors.component.ts
+++ src/app/components/authors/authors.component.ts
@@ -10,6 +10,7 @@ export class AuthorsComponent implements OnInit {
   title = 'List of authors';
   length;
   authors;
+  isDanger = true;
 
   constructor(authorsService: AuthorsService) {
     this.authors = authorsService.getAuthors();
